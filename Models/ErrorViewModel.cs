85222de92ef0b7396878f8d62ed40e27cccc4f9c
NikoloStefanov <123089439+NikoloStefanov@users.noreply.github.com>
2025-01-19 14:49:46 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CulinaryCrossroads1._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
