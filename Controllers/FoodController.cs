e83583c0b75b06402c2c09051c3ac555e8dc53d2
NikoloStefanov <123089439+NikoloStefanov@users.noreply.github.com>
2025-01-20 13:01:48 +0200

    seed data

---
+++ Controllers/FoodController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CulinaryCrossroads1._1.Controllers
+{
+    [Authorize]
+    public class FoodController : Controller
+    {       
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
