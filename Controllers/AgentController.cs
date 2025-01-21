deb161923e524928d1dbe2b58c6ce3e95ec11f95
NikoloStefanov <123089439+NikoloStefanov@users.noreply.github.com>
2025-01-21 23:03:49 +0200

    Added Agent

--- Controllers/AgentController.cs
+++ Controllers/AgentController.cs
@@ -1,15 +1,48 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using CulinaryCrossroads1._1.Models.Agent;
+using CullinaryCrossroads1._1.Core.Services;
+using CullinaryCrossroads1._1.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CulinaryCrossroads1._1.Controllers
 {
     public class AgentController : Controller
     {
-        [Authorize]
-        [HttpPost]
-        public IActionResult Become()
+        private readonly AgentService agentService;
+        public AgentController(AgentService _agentService)
         {
+            agentService = _agentService;
+        }
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Become()
+        {
+            if (await agentService.ExistByIdAsync(User.Id()))
+            {
+                return BadRequest();
+            }
+            
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Become(BecomeAgentFormModel model)
+        {
+            if (await agentService.ExistByIdAsync(User.Id()))
+            {
+                return BadRequest();
+            }
+            if (await agentService.UserWithPhoneNumberExistAsync(model.PhoneNumber))
+            {
+                ModelState.AddModelError(nameof(model.PhoneNumber), "Phone number always exists! Enter another one.");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            await agentService.CreateAsync(User.Id(), model.PhoneNumber);
+
+
+            return RedirectToAction(nameof(FoodController.All), "Food");
         }
     }
 }
---
+++ Models/Agent/BecomeAgentFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CullinaryCrossroads1._1.Infrastructure.DataConstans;
+
+namespace CulinaryCrossroads1._1.Models.Agent
+{
+    public class BecomeAgentFormModel
+    {
+        [Required]
+        [StringLength(Constans.PhoneNumeberMaxLength)]
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
