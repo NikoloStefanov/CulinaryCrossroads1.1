f1046490cc91dc25b147873a34b1415f8c55fc97
NikoloStefanov <123089439+NikoloStefanov@users.noreply.github.com>
2025-01-30 00:16:21 +0200

    update

--- CullinaryCrossroads1.1.Core/Models/Agent/AgentServiceModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CullinaryCrossroads1._1.Core.Models.Agent
-{
-    public class AgentServiceModel
-    {
-        public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-    }
-}
