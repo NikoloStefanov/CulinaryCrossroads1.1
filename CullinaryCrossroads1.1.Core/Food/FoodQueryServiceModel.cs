dcc71a6fb6d50e1d73c026f352ac577e24090940
NikoloStefanov <123089439+NikoloStefanov@users.noreply.github.com>
2025-01-29 21:33:09 +0200

    Add files via upload

---
+++ CullinaryCrossroads1.1.Core/Food/FoodQueryServiceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CullinaryCrossroads1._1.Core.Services
+{
+    public class FoodQueryServiceModel
+    {
+        public int TotalFoodsCount { get; set; }
+        public IEnumerable<FoodServiceModel> Foods { get; set; } = new List<FoodServiceModel>();
+    }
+}
